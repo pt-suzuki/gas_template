7fe1ca8925d187d494708373b2134c61a60ad643
pt-suzuki <>
2022-03-29 17:13:10 +0900

    repository追加

--- src/provider/domains/schedule.ts
+++ src/provider/domains/schedule.ts
@@ -1,6 +1,8 @@
 import { ScheduleTranslator, ScheduleTranslatorImpl } from "~/domains/schedule/schedule_translator";
 import { ScheduleServiceImpl, ScheduleService } from "~/domains/schedule/schedule_service";
+import { ScheduleRepository, ScheduleRepositoryImpl } from "~/domains/schedule/schedule_repository";
 
 export const scheduleTranslator: ScheduleTranslator = new ScheduleTranslatorImpl();
+const scheduleRepository: ScheduleRepository = new ScheduleRepositoryImpl(scheduleTranslator);
 
-export const scheduleService: ScheduleService = new ScheduleServiceImpl(scheduleTranslator);
+export const scheduleService: ScheduleService = new ScheduleServiceImpl(scheduleTranslator, scheduleRepository);
